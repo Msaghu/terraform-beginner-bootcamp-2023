6677aaa30f40e4ec8b146026ded1b3e00e78e26a
Msaghu <maureenmwagoti@gmail.com>
2023-10-09 18:28:22 +0000

    #26 Refactored s3 buckets into the new terrahouse aws modules

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
-#  cloud {
-#    organization = "msaghu"
-#    workspaces {
-#      name = "terra-house-renaissance"
-#    }
-#  }
+  #  cloud {
+  #    organization = "msaghu"
+  #    workspaces {
+  #      name = "terra-house-renaissance"
+  #    }
+  #  }
 }
 
 module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
+  source      = "./modules/terrahouse_aws"
+  user_uuid   = var.user_uuid
   bucket_name = var.bucket_name
 }
